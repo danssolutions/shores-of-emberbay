55e7f9b8c6d55ff8be155ee0f4a1ca8ef745c816
Gierka36 <as.gerardas@gmai.com>
2023-11-01 19:44:02 +0100

    sleep command update

--- CommandWords.cs
+++ CommandWords.cs
@@ -8,7 +8,7 @@ namespace TownOfZuul
 {
     public class CommandWords
     {
-        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "quit", "help", "info", "talk" };
+        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "quit", "help", "info", "talk", "sleep" };
 
         public bool IsValidCommand(string command)
         {
