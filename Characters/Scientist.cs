45843d0e2482aaf892292b50608f59442b69a39e
kornaa <komad23@student.sdu.dk>
2023-12-01 13:15:49 +0100

    scientist dialogue updated

--- Characters/Scientist.cs
+++ Characters/Scientist.cs
@@ -5,15 +5,19 @@ namespace TownOfZuul
         private const string BackText = "Welcome again, Major.";
         public Scientist()
         {
-            Art = GameArt.Scientist;
+            Art = "";
             Text = "Good to see you Major, what can I do for you?";
+
 
             options = new string[]{
 
             "\"Can you tell me more about fish species that I can fish?\"",
-            "\" here will be one more option\"",
+            "\"Can I go with you and your crew on the Research Vessel to the Ocean?\"",
+            //"\"Can you tell me more about sustainable fishing?\"",
+            "\"Can you tell me more about water pollution?\"",
             "\"How can you help me?\"",
             "\"Goodbye\""
+
            };
         }
 
@@ -23,27 +27,27 @@ namespace TownOfZuul
             {
                 case 1:
                     Console.Clear();
-                    //TextChangeMenu textChangeMenu = new();
-                    //textChangeMenu.Display();
-                    //Text = textChangeMenu.ReturnText;
+                    TextChangeMenu textChangeMenu = new();
+                    textChangeMenu.Display();
+                    Text = textChangeMenu.ReturnText;
                     Console.Clear();
                     Console.WriteLine(Art);
                     Console.WriteLine(Text);
                     break;
                 case 2:
                     Console.Clear();
-                    //ReturnTextChangeMenu returnTextChangeMenu = new();
-                    //returnTextChangeMenu.Display();
-                    //Text = returnTextChangeMenu.ReturnText;
+                    ReturnTextChangeMenu returnTextChangeMenu = new();
+                    returnTextChangeMenu.Display();
+                    Text = returnTextChangeMenu.ReturnText;
                     Console.Clear();
                     Console.WriteLine(Art);
                     Console.WriteLine(Text);
                     break;
                 case 3:
                     Console.Clear();
-                    //ReturnTextChangeMenu2 returnTextChangeMenu2 = new();
-                    //returnTextChangeMenu2.Display();
-                    //Text = returnTextChangeMenu2.ReturnText;
+                    ReturnTextChangeMenu2 returnTextChangeMenu2 = new();
+                    returnTextChangeMenu2.Display();
+                    Text = returnTextChangeMenu2.ReturnText;
                     Console.Clear();
                     Console.WriteLine(Art);
                     Console.WriteLine(Text);
@@ -60,17 +64,21 @@ namespace TownOfZuul
             Console.CursorVisible = true;
         }
 
-        public sealed class TextCHangeMenu : Character
+        public sealed class TextChangeMenu : Character
         {
-            public TextCHangeMenu()
+            public TextChangeMenu()
             {
-                Art = GameArt.Scientist;
-                Text = "story of sceintisc here";
+                // Art =
+                Text = "With great plesure. You can do fishing in two location - the Docks and the Ocean.  " +
+                    "/nMy crew and I found around 14 different species in this area. " +
+                    "/nHowever some of the fish are endangered species and by catching them you can contribute to the extinction of the entire population." +
+                    "/nYou have to be careful when assigning villagers";
+
 
                 options = new string[]
                 {
-                    "\"one\"",
-                    "\"second\"",
+                    "\"I would like to hear more about endangered species.\"",
+                    "\"Thank you for your information and time. Now I know what to look for.\"",
                 };
             }
 
@@ -79,10 +87,11 @@ namespace TownOfZuul
                 switch (option)
                 {
                     case 1:
-                        ReturnText = "";
+                        ReturnText = "Endangered species means that the popullaion of that type of fish is drastically dropping or stay at the same low level." +
+                            "/nTo prevent that, fishing for a given species should be limited or completly stopped. ";
                         break;
                     case 2:
-                        ReturnText = "second thing";
+                        ReturnText = "I glad I could help.";
                         break;
                 }
                 continueDisplay = false;
@@ -92,12 +101,12 @@ namespace TownOfZuul
         {
             public ReturnTextChangeMenu()
             {
-                Art = GameArt.Scientist;
-                Text = "here we have more stuff";
-
+                Art = "";
+                Text = "Unfortunately, there is not such a possibility." +
+                    "/nWe do special expeditions only once a month and I can take only trained people on the board.";
                 options = new string[]
                 {
-                "\"here\""
+                "\"Oh, I didn't know that.\""
 
                 };
             }
@@ -106,11 +115,45 @@ namespace TownOfZuul
                 switch (option)
                 {
                     case 1:
-                        ReturnText = "...";
-                        continueDisplay = false; //do we need that?
+                        ReturnText = "I'm I can't help.";
+                        continueDisplay = false;
                         break;
                 }
             }
         }
+        public sealed class ReturnTextChangeMenu2 : Character
+        {
+            public ReturnTextChangeMenu2()
+            {
+                Art = "";
+                Text = "Water pollution in the ocean is caused by the release of harmful substances such as oil spills," +
+                    "\nplastic waste, and chemicals into the water. This pollution can have a devastating effect on the marine life, " +
+                    "\nincluding fish, plants, and other animals living in the ocean. Therefore, it is essential to be mindful of " +
+                    "\nthe substances we release into the ocean to keep it clean and safe for everyone.";
+
+                options = new string[]
+                {
+                    "\"Is there any way to clean the water?\"",
+                    "\"That sounds like a big problem, but I have more important things on my mind\""
+                };
+            }
+
+            public override void ParseOption(int option)
+            {
+                switch (option)
+                {
+                    case 1:
+                        ReturnText = "Actually yes. Somewhere in this area should be algea cleaner which you can use to clean up the water" +
+                            "\nI would recommend going to the Village Elder since she knows the best where everything here is.";
+                        break;
+                    case 2:
+                        ReturnText = "I am always here to help if you change your mind.";
+                        break;
+                }
+                continueDisplay = false;
+
+            }
+        }
     }
+
 }
