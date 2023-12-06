839ad2d0aea509b59fdd4808a1c022a80650e378
kornaa <komad23@student.sdu.dk>
2023-12-06 14:50:51 +0100

    here more stuff

--- Characters/Scientist.cs
+++ Characters/Scientist.cs
@@ -2,16 +2,16 @@ namespace ShoresOfEmberbay
 {
     public class Scientist : Character
     {
-        private const string BackText = "Welcome again, Major.";
+        private const string BackText = "Welcome again, Mayor.";
         public Scientist()
         {
             Art = GameArt.Scientist;
-            Text = "Good to see you Major, what can I do for you?";
+            Text = "Good to see you Mayor, what can I do for you?";
 
 
             options = new string[]{
 
-            "\"Can you tell me more about fish species that I can fish?\"",
+            "\"Can you tell me more about which fish species I can fish?\"",
             "\"Can I go with you and your crew on the Research Vessel to the Ocean?\"",
             "\"Can you tell me more about water pollution?\"",
             "\"How can you help me?\"",
@@ -77,7 +77,7 @@ namespace ShoresOfEmberbay
             public TextChangeMenu()
             {
                 Art = GameArt.Scientist;
-                Text = "With great plesure. You can do fishing in two location - the Docks and the Ocean.  " +
+                Text = "With great pleasure. You can fish in two locations - the Docks and the Ocean.  " +
                     "\nMy crew and I found around 14 different species in this area. " +
                     "\nHowever some of the fish are endangered species and by catching them you can contribute to the extinction of the entire population." +
                     "\nYou have to be careful when going fishing with villagers. ";
@@ -95,11 +95,11 @@ namespace ShoresOfEmberbay
                 switch (option)
                 {
                     case 1:
-                        ReturnText = "Endangered species means that the popullaion of that type of fish is drastically dropping or stay at the same low level." +
+                        ReturnText = "Endangered species means that the popullaion of that type of fish is drastically dropping or stay at the same vulnerable low level." +
                             "\nTo prevent that, fishing for a given species should be limited or completly stopped. ";
                         break;
                     case 2:
-                        ReturnText = "I glad I could help.";
+                        ReturnText = "I am glad I could help.";
                         break;
                 }
                 continueDisplay = false;
@@ -110,8 +110,8 @@ namespace ShoresOfEmberbay
             public ReturnTextChangeMenu()
             {
                 Art = GameArt.Scientist;
-                Text = "Unfortunately, there is not such a possibility." +
-                    "\nWe do special expeditions only once a month and I can take only trained people on the board.";
+                Text = "Unfortunately, there is no such possibility." +
+                    "\nWe do special expeditions only once a month and I take only trained people on board.";
                 options = new string[]
                 {
                 "\"I understand, I didn't know that.\"",
@@ -128,7 +128,7 @@ namespace ShoresOfEmberbay
                         continueDisplay = false;
                         break;
                     case 2:
-                        ReturnText = "I'm sorry I can't help.";
+                        ReturnText = "I am sure you will.";
                         continueDisplay = false;
                         break;
                 }
@@ -140,14 +140,14 @@ namespace ShoresOfEmberbay
             {
                 Art = GameArt.Scientist;
                 Text = "Water pollution in the ocean is caused by the release of harmful substances such as oil spills," +
-                    "\nplastic waste, and chemicals into the water. This pollution can have a devastating effect on the marine life, " +
+                    "\nplastic waste, and chemicals into the water. The pollution can have devastating effects on the marine life, " +
                     "\nincluding fish, plants, and other animals living in the ocean. Therefore, it is essential to be mindful of " +
                     "\nthe substances we release into the ocean to keep it clean and safe for everyone.";
 
                 options = new string[]
                 {
-                    "\"Is there any way to clean up the water?\"",
-                    "\"That sounds like a big problem, but I have more important things on my mind\""
+                    "\"Is there any way to clean the water?\"",
+                    "\"That sounds like a big problem, but I have more important things on my mind.\""
                 };
             }
 
@@ -156,8 +156,8 @@ namespace ShoresOfEmberbay
                 switch (option)
                 {
                     case 1:
-                        ReturnText = "Actually yes. Somewhere in this area should be algea cleaner which you can use to clean up the water" +
-                            "\nI would recommend going to the Village Elder since she should know the best where everything here is.";
+                        ReturnText = "Actually yes. Somewhere in this area it should be possible to get algea cleaner, which you can use to clean the water" +
+                            "\nI would recommend going to the Village Elder since she knows best where everything is.";
                         break;
                     case 2:
                         ReturnText = "I am always here to help if you change your mind.";
@@ -172,12 +172,12 @@ namespace ShoresOfEmberbay
             public ReturnTextChangeMenu3()
             {
                 Art = GameArt.Scientist;
-                Text = "I can share some information about ocean and fishing, ocean pollution and how to prevent it.";
+                Text = "I can share some information about the ocean and fishing, water pollution and how to prevent it.";
                 
                 options = new string[]
                 {
-                     "\"Sound great, I would like to expand knowledge on this subject.\"",
-                     "\"Okey, thank you.\""
+                     "\"Sounds great. I wish to expand my knowledge within this subject.\"",
+                     "\"Okay, thank you.\""
                 };
                 
             }
